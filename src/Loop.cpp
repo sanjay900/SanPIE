b8e5c858a17e1d73ec245a6403f98a2bd3206e40
Sanjay Govind <sanjay.govind9@gmail.com>
2023-02-14 16:01:49 +1300

    tick every millisecond instead of as fast as possible

--- src/Loop.cpp
+++ src/Loop.cpp
@@ -2,26 +2,30 @@
 // Created by sanjay on 7/07/18.
 //
 
-#include <libudev.h>
-#include <cstdio>
-#include <string>
-#include <chrono>
-#include <thread>
-#include <src/output/Output.hpp>
 #include "Loop.hpp"
+
+#include <libudev.h>
+
+#include <chrono>
+#include <cstdio>
+#include <src/output/Output.hpp>
+#include <string>
+#include <thread>
+#include <time.h>
+#include <unistd.h>
 #include "sol.hpp"
 #include "src/input/Input.hpp"
 
 void Loop::tick() {
     long last = std::chrono::duration_cast<std::chrono::microseconds>(
-            std::chrono::system_clock::now().time_since_epoch()
-    ).count();
-    struct udev* udev = udev_new();
+                    std::chrono::system_clock::now().time_since_epoch())
+                    .count();
+    struct udev *udev = udev_new();
     if (!udev) {
         fprintf(stderr, "udev_new() failed\n");
         return;
     }
-    struct udev_monitor* mon = udev_monitor_new_from_netlink(udev, "udev");
+    struct udev_monitor *mon = udev_monitor_new_from_netlink(udev, "udev");
 
     udev_monitor_filter_add_match_subsystem_devtype(mon, "input", NULL);
     udev_monitor_filter_add_match_subsystem_devtype(mon, "tty", NULL);
@@ -30,8 +34,8 @@ void Loop::tick() {
     int fd = udev_monitor_get_fd(mon);
     sol::table devices = lua["devices"];
     sol::table out_devices = lua["v_devices"];
-    std::vector<Input*> sorted_inputs;
-    std::vector<Device*> sorted_devices;
+    std::vector<Input *> sorted_inputs;
+    std::vector<Device *> sorted_devices;
     for (auto &device : devices) {
         sol::table lua_dev = device.second;
         Input &c = lua_dev["dev"];
@@ -48,10 +52,10 @@ void Loop::tick() {
     struct timeval tm;
     tm.tv_sec = 0;
     tm.tv_usec = 0;
-    while(running) {
+    while (running) {
         FD_ZERO(&fds);
         FD_SET(fd, &fds);
-        if (select(fd+1, &fds, NULL, NULL, &tm) > 0) {
+        if (select(fd + 1, &fds, NULL, NULL, &tm) > 0) {
             if (FD_ISSET(fd, &fds)) {
                 struct udev_device *dev = udev_monitor_receive_device(mon);
                 const std::string sysname = udev_device_get_sysname(dev);
@@ -68,15 +72,16 @@ void Loop::tick() {
             }
         }
         long current = std::chrono::duration_cast<std::chrono::microseconds>(
-                std::chrono::system_clock::now().time_since_epoch()
-        ).count();
+                           std::chrono::system_clock::now().time_since_epoch())
+                           .count();
         if (lua["tick"].valid()) {
-            lua["tick"](current-last);
+            lua["tick"](current - last);
         }
         last = current;
         for (auto &device : sorted_inputs) {
             device->tick(lua);
         }
+        usleep(1000);
     }
 }
 void Loop::stop() {
